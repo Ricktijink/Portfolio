d88f04d811db8874436792e3216bbe5d9dd70886
Rick Tijink <ricktijink@hotmail.com>
2018-04-23 15:21:21 +0200

    Responsiveness added, fixed filter active state

--- javascript.js
+++ javascript.js
@@ -1,22 +1,30 @@
-$(document).ready(function(){
-   $(".option").click(function(){
-      var value = $(this).attr("data-filter");
-      if (value == "all") {
-         $(".filter").fadeOut("fast");
-         $(".filter").delay("300").fadeIn("slow");
-      }
-      else {
-         $(".filter").fadeOut("fast");
-         // $(".filter").not("."+value).hide();
-         $(".filter").filter("."+value).delay("300").fadeIn("slow");
-      }
-      // $(".option a").click(function(){
-      //       $(this).addClass("active").siblings().removeClass("active");
-      // })
-   })
-});
+
 
 // Toggle filter menu
 $( ".filter-menu-button" ).click(function() {
   $( ".filter-menu" ).slideToggle( "fast" );
 });
+
+
+$(".option").click(function (){
+  var value = $(this).attr("data-filter");
+  if (value == "all")
+  {
+     $(".filter").fadeOut("fast");
+    $(".filter").delay("300").fadeIn("slow");
+  }
+  else
+  {
+     $(".filter").fadeOut("fast");
+    // $(".filter").not("."+value).hide();
+    $(".filter").filter("."+value).delay("300").fadeIn("slow");
+  }
+ });
+
+ // add active class
+
+  $(".filter-menu li a").click(function(){
+     $(".filter-menu li a").removeClass('active');
+     $(this).addClass('active');
+
+ })﻿
